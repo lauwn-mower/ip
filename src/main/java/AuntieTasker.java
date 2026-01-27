5c6b7185211b026410cb0ea75dfdf3f3af712b4f
wn <lauweina@gmail.com>
2026-01-28 01:22:22 +0800

    feat: Implemented Add, List function for Bot

--- src/main/java/AuntieTasker.java
+++ src/main/java/AuntieTasker.java
@@ -2,13 +2,39 @@ import java.util.Scanner;
 
 public class AuntieTasker {
 
-    public static void main(String[] args){
-        AuntieGreeting.greetUser();
+    public static String[] taskList = new String[100];
+    public static int taskCount = 0;
 
-        System.out.println("");
-        System.out.println("Quick, what you want do?");
+    public static void botAddList(){
+        Scanner input = new Scanner(System.in);
 
-        // Implement Echo function
+        // Keep scanning for inputs unless program exited
+        while (true){
+            String userInput = input.nextLine().trim();
+
+            // Exit program if commanded
+            if (userInput.equalsIgnoreCase("bye")){
+                System.out.println("U finally done ah? Ok stop bothering me now. Shooshoo." );
+                break;
+            }
+
+            // Print list if commanded
+            if (userInput.equalsIgnoreCase("list")){
+                System.out.println("Aiyooo, look at all these tasks. Better get ur bum moving.");
+                for (int i = 0; i < taskCount; i += 1){
+                    System.out.println((i+1) + ". " + taskList[i]);
+                }
+            } else {
+            // If command is neither "list" nor "bye", all inputs are new tasks
+                taskList[taskCount] = userInput;
+                taskCount += 1;
+                System.out.println("Added liao: " + userInput);
+            }
+        }
+    }
+
+    // This method scans for user inputs and echoes back
+    public static void botEcho(){
         String userInput;
         Scanner input = new Scanner(System.in);
         userInput = input.nextLine();
@@ -19,6 +45,15 @@ public class AuntieTasker {
             userInput = input.nextLine();
         }
 
-        System.out.println("U finally done ah? Ok stop bothering me, shooshoo");
+        System.out.println("U finally done ah? Ok stop bothering me, shooshoo.");
+    }
+
+    public static void main(String[] args){
+        AuntieGreeting.greetUser();
+
+        System.out.println("");
+        System.out.println("Quick, what you want do?");
+
+        botAddList();
     }
 }
