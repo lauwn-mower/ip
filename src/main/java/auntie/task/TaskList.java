9c567ee7455ede72d180f9e1e21df1ccfe941ed8
wn <lauweina@gmail.com>
2026-03-06 12:21:17 +0800

    Created TaskList class to make code more OOP

---
+++ src/main/java/auntie/task/TaskList.java
@@ -0,0 +1,50 @@
+package auntie.task;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    /*
+     * TaskList constructors
+     */
+
+    // Construct task list for a NEW user
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    // Construct task list for an EXISTING user
+    // To be used with file retrieval function in main()
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+
+    /*
+     * Section: Methods dealing with editing task list
+     */
+    public void addTask(Task t) {
+        tasks.add(t);
+    }
+
+    public Task deleteTask(int index) {
+        return tasks.remove(index);
+    }
+
+
+    /*
+     * Section: Helper functions to aid logic in tasklist functions
+     */
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+}
