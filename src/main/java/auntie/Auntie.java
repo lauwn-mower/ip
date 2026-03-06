645da0a8dc6cf714deb00de1dfe818186f411f24
wn <lauweina@gmail.com>
2026-03-06 15:08:17 +0800

    created more OOP Auntie class as the main class, before i start migrating from AuntieTasker

---
+++ src/main/java/auntie/Auntie.java
@@ -0,0 +1,17 @@
+package auntie;
+
+public class Auntie {
+
+    public Auntie(String filePath) {
+        //TODO
+    }
+
+    // Main method
+    public void run() {
+        //TODO: make Storage class, Ui Class, Parser Class
+    }
+
+    public static void main(String[] args) {
+//        new Auntie("data/tasks.txt").run();
+    }
+}
