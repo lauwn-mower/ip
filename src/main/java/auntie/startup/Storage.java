2947ce464ea1217901f6b7e3d44f2a126c8d81b1
wn <lauweina@gmail.com>
2026-03-06 15:11:31 +0800

    Updated loadFile() and saveFile() methods

--- src/main/java/auntie/startup/Storage.java
+++ src/main/java/auntie/startup/Storage.java
@@ -30,11 +30,44 @@ public class Storage {
      */
 
     public ArrayList<Task> loadFile() throws AuntieException {
-        //TODO: loadfile
-    }
+        ArrayList<Task> loadedTasks = new ArrayList<>();
+        File f = new File(this.filePath);
+
+        if (!f.exists()) {
+            throw new AuntieException("File doesn't exist leh");
+        }
+
+        try {
+            Scanner s = new Scanner(f);
+            while (s.hasNext()) {
+                // Use your decode helper to turn the text line into a Task object
+                decodeFileString(s.nextLine(), loadedTasks);
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            throw new AuntieException("Cannot find the file!");
+        }
+
+        return loadedTasks;    }
 
     public void saveFile(TaskList taskList) throws IOException, IOException {
-        //TODO: savefile
+        int numberOfTasks = taskList.getSize();
+        File updatedFile = new File(this.filePath);
+
+        // If file missing, create new directory
+        if (updatedFile.getParentFile() != null) {
+            updatedFile.getParentFile().mkdirs();
+        }
+
+        // Open FileWriter with the filePath
+        // Write taskList items to updateFile
+        FileWriter fileWriter = new FileWriter(updatedFile);
+        for (int i = 0; i < numberOfTasks; i += 1) {
+            Task t = taskList.getTask(i);
+            fileWriter.write(t.toFileFormat() + System.lineSeparator());
+        }
+
+        fileWriter.close();
     }
 
 }
