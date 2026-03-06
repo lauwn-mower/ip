ff6203a60060cb3bcbb7220feaeef46a15a60b94
wn <lauweina@gmail.com>
2026-03-06 15:45:14 +0800

    Update Main() Auntie class

--- src/main/java/auntie/Auntie.java
+++ src/main/java/auntie/Auntie.java
@@ -1,16 +1,19 @@
 package auntie;
 
-
-import auntie.startup.Storage;
-import auntie.task.AuntieException;
 import auntie.task.TaskList;
+import auntie.task.AuntieException;
+import auntie.task.Parser;
+import auntie.task.Ui;
+import auntie.startup.Storage;
 
 public class Auntie {
-
+    private Ui ui;
     private Storage storage;
     private TaskList tasks;
 
+    // Constructor for Auntie additionally constructs Ui and Storage object
     public Auntie(String filePath) {
+        ui = new Ui();
         storage = new Storage(filePath);
 
         // File loading process:
@@ -18,17 +21,33 @@ public class Auntie {
         try {
             //
             tasks = new TaskList(storage.loadFile());
+            ui.printWelcome(false);
         } catch (AuntieException e) {
+            ui.printWelcome(true);
             tasks = new TaskList();
         }
     }
 
     // Main method
     public void run() {
-        //TODO: make Storage class, Ui Class, Parser Class
+        boolean isExit = false;
+        Parser parser = new Parser();
+
+        while (!isExit) {
+            try {
+                String fullCommand = ui.readCommand();
+                ui.printLine();
+
+                isExit = parser.parse(fullCommand, tasks, ui, storage);
+            } catch (Exception e) {
+                ui.printError(e.getMessage());
+            } finally {
+                ui.printLine();
+            }
+        }
     }
 
     public static void main(String[] args) {
-//        new Auntie("data/tasks.txt").run();
+        new Auntie("data/tasks.txt").run();
     }
 }
